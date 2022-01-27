32137b324f86186c50f5f93d37cf9110cb5585a5
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-01-27 12:14:11 +0100

    CSS implementado

--- src/php/guardarArchivo.php
+++ src/php/guardarArchivo.php
@@ -1,20 +1,32 @@
-<?php
-    /*Juan Manuel Toscano Reyes <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>*/
-    $directorio='../uploads/'; //Directorio donde se van a subir los ficheros o archivos
-    $tmp_name=$_FILES["fichero"]["tmp_name"]; //Nombre y ruta temporal del fichero
-    $name = basename($_FILES["fichero"]["name"]);
-    $tipo=$_FILES["fichero"]["type"]; //Tipo del archivo subido
-    $tamaño=$_FILES["fichero"]["size"]; //Tamaño del archivo subido
-    if($tipo=="image/png"||$tipo=="image/jpg"||$tipo=="application/pdf"){
-        if($tamaño > 20971520){
-            echo "El tamaño del archivo es muy grande, elija otro archivo más pequeño";
-        }else{
-            $dir_subida=$directorio.$name; //Directorio + nombre del fichero
-            move_uploaded_file($tmp_name, $dir_subida); //Muevo el fichero o archivo de la ruta temporal a el directorio de subida
-            echo 'Archivo subido correctamente';
-        }
-    }else{
-        echo 'El archivo no se ha subido correctamente, utilice una extensión .png .jpg o .pdf';
-    }
-    echo '<br /><a href="../../index.html">Volver al inicio</a>'
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Subida de Archivos</title>
+    </head>
+    <body>
+        <?php
+            $directorio='../uploads/'; //Directorio donde se van a subir los ficheros o archivos
+            $tmp_name=$_FILES["fichero"]["tmp_name"]; //Nombre y ruta temporal del fichero
+            $name = basename($_FILES["fichero"]["name"]);
+            $tipo=$_FILES["fichero"]["type"]; //Tipo del archivo subido
+            $tamaño=$_FILES["fichero"]["size"]; //Tamaño del archivo subido
+            if($tipo=="image/png"||$tipo=="image/jpg"||$tipo=="application/pdf"){
+                if($tamaño > 20971520){
+                    echo "El tamaño del archivo es muy grande, elija otro archivo más pequeño";
+                }else{
+                    $dir_subida=$directorio.$name; //Directorio + nombre del fichero
+                    move_uploaded_file($tmp_name, $dir_subida); //Muevo el fichero o archivo de la ruta temporal a el directorio de subida
+                    echo 'Archivo subido correctamente';
+                }
+            }else{
+                echo 'El archivo no se ha subido correctamente, utilice una extensión .png .jpg o .pdf';
+            }
+            echo '<br /><a href="../../index.html">Volver al inicio</a>'
+        ?>
+    </body>
+</html>
--- src/php/verFicheros.php
+++ src/php/verFicheros.php
@@ -1,16 +1,29 @@
-<?php
-    $directorio='../uploads/';
-    if(is_dir($directorio)){
-        $abrirDirectorio=opendir($directorio);
-        while(($archivo=readdir($abrirDirectorio))!== false){
-            //Se muestran todos los archivos y carpetas excepto "." y ".."
-            if ($archivo != "." && $archivo != "..") {
-                echo $archivo."<br />";
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net">
+        <link rel="stylesheet" href="../css/style.css">
+        <title>Subida de Archivos</title>
+    </head>
+    <body>
+        <?php
+            $directorio='../uploads/';
+            if(is_dir($directorio)){
+                $abrirDirectorio=opendir($directorio);
+                while(($archivo=readdir($abrirDirectorio))!== false){
+                    //Se muestran todos los archivos y carpetas excepto "." y ".."
+                    if ($archivo != "." && $archivo != "..") {
+                        echo $archivo."<br />";
+                    }
+                }
+                closedir($abrirDirectorio);
+            }else{
+                echo 'Esto no es una ruta válida';
             }
-        }
-        closedir($abrirDirectorio);
-    }else{
-        echo 'Esto no es una ruta válida';
-    }
-    echo '<a href="../../index.html">Volver al inicio</a>'
-?>
+            echo '<a href="../../index.html">Volver al inicio</a>'
+        ?>
+    </body>
+</html>
